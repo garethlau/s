374e48a334edbd804d46cc6b8e2f95804c83809c
Gareth <Gareth.kh.lau@gmail.com>
2020-08-14 08:05:58 -0600

    Define constants

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+export const Colors = {
+  RED: "#EF0702",
+  PINK: "#FFC2C0",
+  WHITE: "#FFFFFF",
+  GREEN: "#128469",
+  BLACK: "#000000",
+  BLUE: "#009DD6",
+  PURPLE: "#7944BB",
+  GREY: "#F5F6FC",
+};
+
+export const FontSizes = {
+  HEADING: "60px",
+  BODY: "30px",
+};
